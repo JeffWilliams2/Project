4edcc4752f852285ea76e9b721f91d0ac12f3f36
JeffWilliams2 <68485116+JeffWilliams2@users.noreply.github.com>
2025-02-15 23:08:48 -0600

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 
 // Typing Effect
 var typed = new Typed('#wrd', {
-    strings: ['Python Program','C Program', 'C++ Program', 'Java Program', 'Full-stack developer'],
+    strings: ['Data Engineer', 'Python Developer', 'Data Analyst', 'Financial Analyst', 'Cloud Practitioner'],
     typeSpeed: 100,
     loop: true
 });
@@ -19,24 +19,6 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     navLinks.classList.toggle('active');
 });
-
-// popup window
-function openPopup() {
-    document.getElementById('popup').style.display = 'flex';
-}
-
-function closePopup() {
-    document.getElementById('popup').style.display = 'none';
-}
-
-function copyUPI() {
-    var upiId = document.getElementById('upi-id').textContent;
-    navigator.clipboard.writeText(upiId).then(function() {
-        alert('UPI ID copied to clipboard');
-    }, function() {
-        alert('Failed to copy UPI ID');
-    });
-}
 
 // footer 
 document.getElementById("currentYear").textContent = new Date().getFullYear();
@@ -55,37 +37,3 @@ document.addEventListener('DOMContentLoaded', function () {
     window.addEventListener('scroll', checkFooterVisibility);
     checkFooterVisibility();
 });
-
-// my skill 
-const slides = document.querySelector('.slides');
-const images = document.querySelectorAll('.slides img');
-
-let index = 0;
-const slideInterval = 2000;
-
-function updateSlidePosition() {
-    slides.style.transform = `translateX(-${index * 100}%)`;
-}
-
-document.querySelector('.next').addEventListener('click', () => {
-    index = (index + 1) % images.length;
-    updateSlidePosition();
-});
-
-document.querySelector('.prev').addEventListener('click', () => {
-    index = (index - 1 + images.length) % images.length;
-    updateSlidePosition();
-});
-
-function autoSlide() {
-    index = (index + 1) % images.length;
-    updateSlidePosition();
-}
-
-setInterval(autoSlide, slideInterval);
-
-
-// This code was created by WerDeveloper.
-// Unauthorized copying, distribution, or modification of this code, in whole or in part, is strictly prohibited without prior written permission.
-// Please do not remove or alter the credit to the original creator. If you wish to use this code for personal or commercial purposes, kindly contact the creator for permissions.
-// Thank you for respecting the work and effort that went into creating this code.
